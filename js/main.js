51a489a5af9f1ac548ac0510800007708a1bc30d
enfacee <enfac@mail.ru>
2014-12-04 17:28:22 +0500

    Водительское

--- js/main.js
+++ js/main.js
@@ -1,5 +1,33 @@
 define(["jquery"], function ($) {
 $(function () {
-	$(".cardContents").text("Hello, world!");
+$(document).ready(function() {
+	function edit(){
+		$text = $(this);
+
+		$enter_text = $("<input class = 'text' type = 'text' value =" + $(this).text() +"></input>");
+		$(this).replaceWith($enter_text);
+
+		$enter_text.focus();
+
+		$enter_text.blur( function() {
+			if ($(this).val().length > 0) {
+				$text.text($(this).val());
+			}
+			$enter_text.replaceWith($text);
+			$text.click(edit);
+		});
+
+	}
+
+	$(".text").click(edit);
+	$(".text_3").click(edit);
+	$(".text_4").click(edit);
+	$(".category").click(edit);
+
+
+
+
+});
+
 });
 });
