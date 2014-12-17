81307d5b60428676c717bfaec6a7e70edd3350f8
enfacee <enfac@mail.ru>
2014-12-17 17:40:50 +0500

    Добавил фото с веб-камеры

--- js/main.js
+++ js/main.js
@@ -18,7 +18,61 @@ $(document).ready(function() {
 		});
 
 	}
+	var
+		photo = $("#photo")[0],				//canvas для фото
+		fContext = photo.getContext("2d"),
+		video = $("#cam")[0],
+		videoStreamUrl = false,
+		localStream;
+                                     		                                     
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+     		     
+     		     
+    function start() {	
+ 	   		 	   
+ 	    navigator.getUserMedia (
+		{
+			video: true, 
+		},
+ 		function (localMediaStream) {
+							
+ 			window.URL.createObjectURL = window.URL.createObjectURL || window.URL.webkitCreateObjectURL || window.URL.mozCreateObjectURL || window.URL.msCreateObjectURL;
+ 					// получаем url поточного видео
+ 			videoStreamUrl = window.URL.createObjectURL(localMediaStream);
+					//устанавливаем как источник для video
+ 			video.src = videoStreamUrl;
+ 			localStream = localMediaStream;
+			},
+ 		   //если ошибка
+		   function(){}
+ 	   );
+   };
+	
+	function capture() {
 
+         if (!videoStreamUrl) {
+             alert("Camera is off!");
+             return;
+         }
+         		         
+        var width = photo.width;
+        var heigth = photo.height;
+        fContext.drawImage(video, 0,0,width,heigth);
+        localStream.stop();
+		$(".panel").hide();
+		
+     }
+	
+	
+	
+	
+	$("#photo").click( function () {
+	
+		$(".panel").show();
+	})
+	$("#cam_photo").click(start);
+	$("#take_photo").click(capture);
+	
 	$(".text").click(edit);
 	$(".text_3").click(edit);
 	$(".text_4").click(edit);
